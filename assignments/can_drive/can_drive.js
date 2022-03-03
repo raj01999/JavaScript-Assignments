3a97fb5481b9d723fc5d04e8fae42007f9e3b37d
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-03-03 11:21:19 +0530

    CanDrive

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,17 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if ( !hasDrivingLiscence ){
+        return "You cannot drive"
+    }
+    if ( isTired ){
+        return "You shouldn't drive"
+    }
+    if ( !isSober ){
+        return "You shouldn't drive"
+    }
+    else{
+        return "You can drive"
+    }
 }
 
 module.exports = CanDrive;
