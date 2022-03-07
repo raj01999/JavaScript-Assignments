1c00a64467e0a964b9fa11c9eb5e1b4f985ce368
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-03-07 19:08:18 +0530

    checking type of mass and height

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,7 +1,7 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
-    if (mass<=0 || height<=0){
+    if (typeof mass !== Number || typeof height !== Number || mass <= 0 || height <= 0) {
         return "INVALID INPUT"
     }
     return (mass / (height * height))
