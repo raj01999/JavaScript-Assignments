44d115a87cbcec12bf6547e7c70c3b1777c5f816
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-03-10 01:11:22 +0530

    pending and completed

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -2,21 +2,28 @@
 let todoList;
 featchItem();
 
-document.querySelector("#btn1").addEventListener("click", ()=>{
+
+// EventListener for All todoList button
+document.querySelector("#btn1").addEventListener("click", () => {
     loadItems(todoList);
 });
 
-document.querySelector("#btn2").addEventListener("click", ()=>{
-    let completedTodoList = todoList.filter(ele=>ele.completed);
+
+// EventListener for Completed todoList button
+document.querySelector("#btn2").addEventListener("click", () => {
+    let completedTodoList = todoList.filter(ele => ele.completed);
     loadItems(completedTodoList);
 });
 
-document.querySelector("#btn3").addEventListener("click", ()=>{
-    let completedTodoList = todoList.filter(ele=>!ele.completed);
-    loadItems(completedTodoList);
+
+// // EventListener for Pending todoList button
+document.querySelector("#btn3").addEventListener("click", () => {
+    let pendingTodoList = todoList.filter(ele => !ele.completed);
+    loadItems(pendingTodoList);
 });
 
 
+// Featching data from api
 function featchItem() {
     let xml = new XMLHttpRequest;
     xml.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
@@ -30,16 +37,24 @@ function featchItem() {
 }
 
 
+// Loading data to webPage
 function loadItems(list) {
     let todoArea = document.querySelector("#todoArea");
     let html = "";
-    list.forEach(ele=>{
+    list.forEach(ele => {
+        let val
+        if (ele.completed) {
+            val = "Completed"
+        }
+        else {
+            val = "Pending"
+        }
         html += `<div class="card" style="width: 18rem; margin: 10px">
         <div class="card-body">
             <h5 class="card-title">${ele.title}</h5>
             <p class="card-text">User Id :  ${ele.userId}</p>
             <p class="card-text">Id :  ${ele.id}</p>
-            <p class="card-text">Status : ${ele.completed}</p>
+            <p class="card-text">Status : ${val}</p>
         </div>
         </div>`;
     });
